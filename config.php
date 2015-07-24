95c0ef97760c13bef4191fed189b98f8f472095a
blakeFez <heiyusysu@qq.com>
2015-07-24 15:21:07 +0800

    change

--- config.php
+++ config.php
@@ -3,6 +3,3 @@
 define('DEFAULTCONTROLLER','chat');
 //默认action
 define('DEFAULTACTION','index');
-//企业qq广播
-define('QQMSGSEND','https://qq.4399houtai.com/open/api/send_msg.php');
-define('BROADCAST','https://qq.4399houtai.com/open/api/broadcast.php');
